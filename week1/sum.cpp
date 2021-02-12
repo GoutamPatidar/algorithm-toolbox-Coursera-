a184db3041ca175c98f434d94fcb063064106788
Goutam Patidar <73788341+Goutampatidar@users.noreply.github.com>
2021-02-12 13:01:02 +0530

    Add files via upload

---
+++ week1/sum.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ll a,b;
+    cin>>a;// enter first no...
+    cin>>b;// enter second no...
+    cout<<(a+b)<<endl;
+}
